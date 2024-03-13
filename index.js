9cee970f7921b0d991f41de68552537a6a751717
Dima Linker <daniel.linker@mailbox.org>
2024-03-13 13:41:49 +0100

    Get current time

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 import store from "./redux/store.js";
+import getCurrentTime from "./utils/getCurrentTime.js";
 
-const newState = store.subscribe(() => {
-    console.log(store.getState())
+
+const addTimeBtn = document.getElementById("addTime");
+addTimeBtn.addEventListener("click", () => {
+    store.dispatch({
+        type: "ADD_CURRENT_TIME", payload: getCurrentTime()});
+});
+
+const timeList = document.getElementById("timeList");
+store.subscribe(() => {
+    timeList.innerHTML = store.getState().map(time => {
+        return `<li>${time}</li>`
+    }).join("");
 })
-store.dispatch({type: "ADD_CURRENT_TIME", payload: "11:11:11"});
-newState();
-store.dispatch({type: "CLEAR_ALL_TIMES"});
+
+
+const clearTimesBtn = document.getElementById("clearTimes");
+clearTimesBtn.addEventListener("click", () => {
+    store.dispatch({type: "CLEAR_ALL_TIMES"});
+});
+
+
+
+
+
+
+
+// const newState = store.subscribe(() => {
+//     console.log(store.getState())
+// })
+// store.dispatch({type: "ADD_CURRENT_TIME", payload: "11:11:11"});
+// newState();
+// store.dispatch({type: "CLEAR_ALL_TIMES"});
